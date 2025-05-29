121c9f84ad6a97fc0242a9af8492647f122f2e97
Lawrence Caulker <locaulker@gmail.com>
2025-05-29 18:39:54 -0400

    add navigator and clioboard functionalites

--- src/SingleColor.jsx
+++ src/SingleColor.jsx
@@ -1,11 +1,27 @@
+import { toast } from "react-toastify"
+
 const SingleColor = ({ index, color }) => {
   const { hex, weight } = color
   // console.log(color)
+
+  const saveToClipboard = async () => {
+    if (navigator.clipboard) {
+      try {
+        await navigator.clipboard.writeText(`#${hex}`)
+        toast.success("Color Value Copied to Clipboard")
+      } catch (error) {
+        toast.error("Failed to copy Color Value")
+      }
+    } else {
+      toast.error("Browser Clipboard Access Denied")
+    }
+  }
 
   return (
     <article
       className={index > 10 ? "color color-light" : "color"}
       style={{ background: `#${hex}` }}
+      onClick={saveToClipboard}
     >
       <p className="percent-value">{weight}%</p>
       <p className="color-value">#{hex}</p>
